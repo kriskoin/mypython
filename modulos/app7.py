ec9abfcca22ead7d8cb734565431ab31a832f82e
Kriskoin <cris7775@gmail.com>
2024-09-29 17:48:21 -0600

    add: Paquetes con nombre dinamico

---
+++ modulos/app7.py
@@ -0,0 +1,20 @@
+"""ultimate python"""
+"""https://www.udemy.com/course/ultimate-python-de-cero-a-programador-experto/"""
+""" Lecture 93 """
+
+from  usuarios.impuestos.gravamenes import pagar_impuestos
+
+
+#print(usuarios.__name__)
+#print(usuarios.__package__)
+#print(usuarios.__path__)
+#print(usuarios.__file__)
+
+#print(usuarios.impuestos.__name__)
+#print(usuarios.impuestos.__package__)
+#print(usuarios.impuestos.__path__)
+#print(usuarios.impuestos.__file__)
+
+pagar_impuestos()
+
+print(__name__)
--- modulos/usuarios/impuestos/gravamenes.py
+++ modulos/usuarios/impuestos/gravamenes.py
@@ -1,9 +1,13 @@
 """ultimate python"""
 """https://www.udemy.com/course/ultimate-python-de-cero-a-programador-experto/"""
-""" Lecture 91 """
+""" Lecture 91 ,93"""
 
 from ..gestion.crud import crud_guardar
-
+print (__name__)
 def pagar_impuestos():
     print ("Pagando impuestos")
     crud_guardar()
+
+
+if __name__ == "__main__":
+    print("Tarea de mantenimiento")
