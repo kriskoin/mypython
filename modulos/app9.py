db667ea1179a29374c30ec3e8a7b1318d60cb8d7
Kriskoin <cris7775@gmail.com>
2024-09-29 17:53:07 -0600

    add: imports condicionales

---
+++ modulos/app9.py
@@ -0,0 +1,14 @@
+"""ultimate python"""
+"""https://www.udemy.com/course/ultimate-python-de-cero-a-programador-experto/"""
+""" Lecture 94 """
+
+if __name__ != "__main__": 
+    from  usuarios.impuestos.gravamenes import pagar_impuestos
+
+
+    pagar_impuestos()
+
+    print(__name__)
+
+if __name__ == "__main__": 
+    print("Tareas de mantenimiento")
