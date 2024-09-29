ff5a437e30a4bc91136d03fc05618e47ad607da7
Kriskoin <cris7775@gmail.com>
2024-09-29 16:34:39 -0600

    add: Lesson sub paquetes

---
+++ modulos/app4.py
@@ -0,0 +1,7 @@
+"""ultimate python"""
+"""https://www.udemy.com/course/ultimate-python-de-cero-a-programador-experto/"""
+""" Lecture 90"""
+
+from  usuarios.acciones.utilidades import util_guardar,util_pagar_impuestos
+
+util_pagar_impuestos()
---
+++ modulos/usuarios/acciones/utilidades.py
@@ -0,0 +1,9 @@
+"""ultimate python"""
+"""https://www.udemy.com/course/ultimate-python-de-cero-a-programador-experto/"""
+""" Lecture 90 """
+
+def util_guardar():
+    print("Sub PAQUETE: guardando")
+
+def util_pagar_impuestos():
+    print("Sub PAQUETE: pagando impuestos")
