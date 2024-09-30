04541d6f7a0607e8a9f431dbb990c08885f51e62
Kriskoin <cris7775@gmail.com>
2024-09-29 20:47:18 -0600

    add: lecture 97 , inyeccion dependencias

---
+++ rutas/03_inyeccion_deps.py
@@ -0,0 +1,22 @@
+"""ultimate python"""
+"""https://www.udemy.com/course/ultimate-python-de-cero-a-programador-experto/"""
+""" Lecture 97 """
+
+
+# import correa
+
+# class Perro:
+#     def __init__(self,Correa):
+#         self.correa = Correa()
+
+
+# import usuario
+
+# def guardar():
+#     usuario.guardar()
+
+# def guardar(entidad):
+#     entidad.guardar()
+
+
+
