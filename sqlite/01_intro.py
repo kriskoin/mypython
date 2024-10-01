0584f3abfbe425aa1c5fc816c6027fe968e8f17c
Kriskoin <cris7775@gmail.com>
2024-09-30 19:22:48 -0600

    add: lecture 106

---
+++ sqlite/01_intro.py
@@ -0,0 +1,12 @@
+#
+# Course: ultimate python
+# https://www.udemy.com/course/ultimate-python-de-cero-a-programador-experto/
+# Lecture 106
+#
+
+import sqlite3
+
+con = sqlite3.connect("app.db")
+
+con.close()
+
