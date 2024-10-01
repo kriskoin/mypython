3c494313ad76c27c21bd8b688fc4f5fac89eb42d
Kriskoin <cris7775@gmail.com>
2024-09-30 19:43:14 -0600

    add: lecture 111

---
+++ sqlite/06_select_one.py
@@ -0,0 +1,15 @@
+#
+# Course: ultimate python
+# https://www.udemy.com/course/ultimate-python-de-cero-a-programador-experto/
+# Lecture 111
+#
+
+import sqlite3
+
+with sqlite3.connect("app.db") as  con:
+    cursor = con.cursor()
+   
+    cursor.execute(
+            "SELECT * from usuarios"
+                )
+    print(cursor.fetchone())
