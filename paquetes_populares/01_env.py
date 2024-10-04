ad3925855f55bc7dba6672e3aa1f80f953f5d06b
Kriskoin <cris7775@gmail.com>
2024-10-03 21:58:21 -0600

    add: Lecture 130

---
+++ paquetes_populares/01_env.py
@@ -0,0 +1,9 @@
+#
+# Course: ultimate python
+# https://www.udemy.com/course/ultimate-python-de-cero-a-programador-experto/
+# Lecture 130
+#
+
+import os
+my_key = os.environ.get("SEDNGRID_API_KEY")
+print(my_key)
