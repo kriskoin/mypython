5f3eae60563d1df7f14a1c77be342b23a38c6d71
Kriskoin <cris7775@gmail.com>
2024-10-05 14:48:50 -0600

    add: lecture 143

--- productly/productos/models.py
+++ productly/productos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=255)
+
+class Poeducto(models.Model):
+    nombre = models.CharField(max_length=255)
+    stock = models.IntegerField()
+    puntaje = models.FloatField()
+    categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
+
+                  
